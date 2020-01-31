2005701d80fedba161f558324ab2567ffdc070fc
LuiD2 <47342745+LuiD2@users.noreply.github.com>
2020-01-31 14:40:39 -0700

    Added database

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+var databaseUrl = "mongodb+srv://Admin:Admin123@cluster-hsisi.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(databaseUrl, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(() => console.log("Connected to Database"))
+.catch(err => {console.log(Error, err.message) });
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+
+
+
 const router = express.Router();
 
 router.route("/").get(
