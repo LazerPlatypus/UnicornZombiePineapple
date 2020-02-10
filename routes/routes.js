190e364db328d84f2be08d91d62dda9955bb0ef6
LazerPlatypus(Laptop) <aholt@student.neumont.edu>
2020-02-10 14:32:24 -0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongo_controller = require('../scripts/mongo_controller.js');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
