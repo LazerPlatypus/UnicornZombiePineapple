4493b8863ed67dd04bd2fdd969a75699e79066d8
VegaNan <47089431+VegaNan@users.noreply.github.com>
2020-02-19 13:51:43 -0700

    Update game.js
    square

--- public/game.js
+++ public/game.js
@@ -2,10 +2,40 @@ var zombies = [];
 var canvas;
 var ctx;
 var pineapples;
+const types = {
+    WALL : 1,
+    PINEAPPLE : 2,
+    UNICORN: 3,
+    ZOMBIE: 4
+}
 
 menu();
 
+class Button{
+    constructor(func, x, y, w, h, text){
+        var obj = this;
+        this.eventListener = canvas.addEventListener("click", function (e) {            // Handle Click on Canvas
+            obj.clickEvent(e);
+        });
+        var fillStyleBtn = 'rgba(255,255,255,255)';
+        ctx.beginPath();
+        ctx.rect(x, y, w, h);
+        ctx.fillStyle = fillStyleBtn;
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
+        ctx.closePath();
 
+        ctx.font = "48px serif";
+        ctx.textAlign = "center";
+        ctx.fillStyle = '#000000';
+        ctx.fillText(Text, canvas.width / 2, y+45);
+        ctx.font = "38px serif";
+    
+        ctx.fillText(text, canvas.width / 2, 165);
+    }
+}
 function menu() {
     var option1 = {
         x: 150,
@@ -79,6 +109,7 @@ function menu() {
     ctx.fillStyle = '#000000';
     ctx.fillText("Menu", canvas.width / 2, 70);
     ctx.font = "38px serif";
+
     ctx.fillText("Play Solo Game", canvas.width / 2, 165);
     ctx.fillText("Start Multiplayer Game", canvas.width / 2, 255);
     ctx.fillText("Join Multiplayer Game", canvas.width / 2, 345);
@@ -92,23 +123,25 @@ function menu() {
         };
     }
 
-    
 
-    canvas.addEventListener('click', function (evt) {
-        var mousePos = getMousePos(canvas, evt);
-        // debugger;
-        if (isInside(mousePos, option1)) {
-            clearCanvas();
-            drawGameBoard();
-        } else if (isInside(mousePos, option2)) {
-            clearCanvas();
-            drawGameBoard();
-        } else if (isInside(mousePos, option3)) {
-            alert('do something here to type in the code');
-        } else {
-            alert('clicked outside rect');
+
+    canvas.addEventListener('click', 
+    function (evt){
+            var mousePos = getMousePos(canvas, evt);
+            // debugger;
+            if (isInside(mousePos, option1)) {
+                clearCanvas();
+                drawGameBoard();
+            } else if (isInside(mousePos, option2)) {
+                clearCanvas();
+                drawGameBoard();
+            } else if (isInside(mousePos, option3)) {
+                alert('do something here to type in the code');
+            } else {
+                alert('clicked outside rect');
+            }
         }
-    }, false);
+    , false);
 
     function isInside(pos, rect) {
         return pos.x > rect.x && pos.x < rect.x + rect.width && pos.y < rect.y + rect.height && pos.y > rect.y
@@ -121,15 +154,14 @@ function clearCanvas() {
         var ctx = BOARD_CANVAS_CONTEXT;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
-    canvas.removeEventListener
 }
 
 function drawGameBoard() {
     var img = new Image;
-    img.src = "/boardBackground.png";
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    }
+    // img.src = "/boardBackground.png";
+    // img.onload = function () {
+    //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    // }
     
     drawObstacles();
 }
@@ -164,12 +196,6 @@ class Zombie {
     }
 }
 
-const types = {
-    WALL,
-    PINEAPPLE,
-    UNICORN,
-    ZOMBIE
-}
 //this is for the walls and pineapples
 function component(width, height, type, x, y){
     this.width = width;
@@ -183,7 +209,8 @@ function component(width, height, type, x, y){
     this.update = function() {
 
         if(this.type == types.WALL){
-            ctx.fillStyle = '#696969';
+            console.log("wall");
+            ctx.fillStyle = 'red';
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }else if(this.type == types.ZOMBIE){
             var img = new Image;
