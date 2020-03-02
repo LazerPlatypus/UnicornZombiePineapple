b5a530d2a327210ceb8a80f40f4d9023465e0d3d
LazerPlatypus(Laptop) <aholt@student.neumont.edu>
2020-03-02 13:56:20 -0700

    Revert "Revert "Working Login and register""
    This reverts commit 270b2f9453d32f2ab162ac9f989c15a3a30610e8.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const session = require("express-session");
 var routes = require("./routes/routes");
 const path = require('path')
+const bcrypt = require('bcrypt');
 const app = express();
 let port = 3000;
 
